7c897f238f4a192a15c666622462860f613714fb
Ewerton Souza <139550812+esouza1992@users.noreply.github.com>
2024-12-05 06:58:02 +0000

    FIX: Build

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,4 +1,4 @@
-import { MembershipFields } from "@/enums/membership";
+import { MembershipFields } from "../enums/membership";
 
 export interface ICommon {
   id?: string;
@@ -18,30 +18,7 @@ export interface IAlbumDTO extends IAlbum, ICommon {}
 export interface ICommonDTO extends IImageDTO, ICommon {}
 
 export type MemberPayload = {
-  [MembershipFields.Photo]?: IMemberPhoto;
-  [MembershipFields.Id]?: string;
-  [MembershipFields.Fullname]: string;
-  [MembershipFields.BirthDate]?: string;
-  [MembershipFields.Gender]?: number;
-  [MembershipFields.MaritalStatus]?: number;
-  [MembershipFields.PostalCode]?: string;
-  [MembershipFields.Address]?: string;
-  [MembershipFields.City]?: string;
-  [MembershipFields.County]?: string;
-  [MembershipFields.State]?: string;
-  [MembershipFields.MotherName]?: string;
-  [MembershipFields.FatherName]?: string;
-  [MembershipFields.SpouseName]?: string;
-  [MembershipFields.WeddingDate]?: string;
-  [MembershipFields.Children]?: string[];
-  [MembershipFields.BaptismChurch]?: string;
-  [MembershipFields.BaptismDate]?: string;
-  [MembershipFields.AdmissionType]?: string;
-  [MembershipFields.BaptizedPastor]?: string;
-  [MembershipFields.AdmissionDate]?: string;
-  [MembershipFields.Congregation]?: string;
-  [MembershipFields.ChurchRole]?: string;
-  [MembershipFields.BelongsTo]?: string;
+  [key in MembershipFields]?: IMemberPhoto | string | number | string[];
 };
 
 export interface IMemberPhoto {
